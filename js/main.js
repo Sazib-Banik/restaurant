56b03a6925b860e92de51f6c1285c281465fb7aa
Sazib-Banik <sazib.banik.1@gmail.com>
2022-11-21 14:38:46 +0600

    chef area done

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:30,
+    responsiveClass:true,
+    autoplay:true,
+    autoplayTimeout:1000,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:1.5,
+            nav:false,
+            dots: true,
+            loop:true
+        }
+    }
+})
